7d6afec9affeb46c09dc0a7f2d547c3f54c416fa
shwnyao <shawn.swyao@gmail.com>
2021-08-05 14:44:57 +0800

    feat(torch_nn): allow model to be saved and use inference later, also allow more params of nn to be configured

--- plb/optimizer/solver_torch_nn.py
+++ plb/optimizer/solver_torch_nn.py
@@ -1,34 +1,26 @@
 import taichi as ti
 import numpy as np
+import pandas as pd
+import seaborn as sns
 from yacs.config import CfgNode as CN
 import torch
 from torch import nn
 
-from .optim import Optimizer, Adam, Momentum
+from .optim import Optimizer
 from ..engine.taichi_env import TaichiEnv
 from ..config.utils import make_cls_config
 
-OPTIMS = {
-    'Adam': Adam,
-    'Momentum': Momentum
-}
-
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, l1=256, l2=256, activation=nn.Tanh):
         super(MLP, self).__init__()
         self.linear_tanh_stack = nn.Sequential(
-            nn.Linear(input_dim, 256),
-            nn.Tanh(),
-            nn.Linear(256, 256),
-            nn.Tanh(),
-            nn.Linear(256, output_dim)
+            nn.Linear(input_dim, l1),
+            activation(),
+            nn.Linear(l1, l2),
+            activation(),
+            nn.Linear(l2, output_dim)
         )
-        self.learning_rate = 1e-4
-        self.batch_size = 64
-        self.epochs = 5
-        self.optimizer = torch.optim.Adam(
-            self.parameters(), lr=self.learning_rate)
 
     def forward(self, x):
         logits = self.linear_tanh_stack(x)
@@ -38,12 +30,14 @@ class MLP(nn.Module):
 class SolverTorchNN:
     def __init__(self, env, logger=None, cfg=None, **kwargs):
         self.cfg = make_cls_config(self, cfg, **kwargs)
-        self.optim_cfg = self.cfg.optim
         self.env = env
         self.logger = logger
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.nn = MLP(env.observation_space.shape[0], env.action_space.shape[0]).double(
         ).to(self.device)
+        self.learning_rate = self.cfg.optim.lr
+        self.optimizer = torch.optim.Adam(
+            self.nn.parameters(), lr=self.learning_rate)
 
     def train(self):
         if self.logger is not None:
@@ -52,7 +46,6 @@ class SolverTorchNN:
         actions = []
         obs = self.env.reset()
         taichi_env.set_copy(False)
-        print("=======================")
         with ti.Tape(loss=taichi_env.loss.loss):
             for i in range(self.cfg.horizon):
                 state_tensor = torch.as_tensor(obs).to(self.device)
@@ -65,42 +58,46 @@ class SolverTorchNN:
                     self.logger.step(
                         None, None, reward, None, i == self.cfg.horizon-1, loss_info)
         loss = taichi_env.loss.loss[None]
-        print('loss: ', loss)
 
         grads = taichi_env.primitives.get_grad(self.cfg.horizon)
         for action, grad in zip(actions, grads):
             grad_tensor = torch.as_tensor(grad).to('cuda')
             action.backward(grad_tensor, retain_graph=True)
 
-        self.nn.optimizer.step()
+        self.optimizer.step()
         actions_np = [t.data.cpu().numpy() for t in actions]
         return loss, actions_np
 
-    def solve(self, init_actions=None, callbacks=()):
+    def solve(self, callbacks=()):
         best_actions = None
         best_loss = 1e10
         for iter in range(self.cfg.n_iters):
-            self.nn.optimizer.zero_grad()
+            self.optimizer.zero_grad()
             loss, actions = self.train()
 
             if loss < best_loss:
                 best_loss = loss
                 best_actions = actions.copy()
-            # for callback in callbacks:
-            #     callback(self, optim, loss, grad)
+                torch.save(self.nn.state_dict(), 'model_weights.pth')
+            for callback in callbacks:
+                callback(loss, actions)
 
         self.env.reset()
-        print("actions: ", best_actions)
         return best_actions
 
-    @staticmethod
-    def init_actions(env, cfg):
-        action_dim = env.primitives.action_dim
-        horizon = cfg.horizon
-        if cfg.init_sampler == 'uniform':
-            return np.random.uniform(-cfg.init_range, cfg.init_range, size=(horizon, action_dim))
-        else:
-            raise NotImplementedError
+    def inference(self):
+        self.nn.load_state_dict(torch.load('model_weights.pth'))
+        self.nn.eval()
+        actions = []
+        obs = self.env.reset()
+        for i in range(self.cfg.horizon):
+            state_tensor = torch.as_tensor(obs).to(self.device)
+            action_var = self.nn(state_tensor)
+            actions.append(action_var)
+            action_np = action_var.data.cpu().clone().numpy()
+            obs, reward, done, loss_info = self.env.step(action_np)
+        actions_np = [t.data.cpu().numpy() for t in actions]
+        return actions_np
 
     @classmethod
     def default_config(cls):
@@ -126,9 +123,21 @@ def solve_torch_nn(env, path, logger, args):
                            n_iters=(args.num_steps + T-1)//T, softness=args.softness, horizon=T,
                            **{"optim.lr": args.lr, "optim.type": args.optim, "init_range": 0.0001})
 
-    actions = solver.solve()
+    def log_loss(loss, actions):
+        print('loss: ', loss)
 
+    actions = solver.solve(callbacks=[log_loss])
+    # actions = solver.inference()
+
+    rewards = np.zeros((T,))
     for idx, act in enumerate(actions):
-        env.step(act)
+        _, reward, _, _ = env.step(act)
+        rewards[idx] = reward
         img = env.render(mode='rgb_array')
         cv2.imwrite(f"{path}/{idx:04d}.png", img[..., ::-1])
+
+    df = pd.DataFrame(dict(time=np.arange(T),
+                           value=np.cumsum(rewards)))
+    g = sns.relplot(x="time", y="value", kind="line", data=df)
+    g.fig.autofmt_xdate()
+    g.savefig("output.png")
