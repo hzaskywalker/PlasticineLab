2659b89eca081a1cdd6bbda1abbd6dc305daee57
RexSkywalkerLee <tingxiangfan@gmail.com>
2021-09-02 23:20:35 +0800

    [Feat] add reward prediction experiment, pre-release, bug expected

---
+++ plb/algorithms/reward_prediction/test.py
@@ -0,0 +1,3 @@
+import os
+print(os.path.abspath(''))
+print(__file__)
--- plb/engine/losses/__init__.py
+++ plb/engine/losses/__init__.py
@@ -2,4 +2,5 @@ from .loss import Loss
 from .state_loss import StateLoss
 from .chamfer_loss import ChamferLoss
 from .emd_loss import EMDLoss
+from .pred_loss import PredLoss
 from .emd_module import compute_emd, emdModule
