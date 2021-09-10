Fixed Loss Average Bug

plb/mpi/mpi_pytorch.py
@@ -1,8 +1,9 @@
 from typing import Union, Generator
+import numpy as np
 from numpy import ndarray
 import torch
 from torch import Tensor, nn
-from .mpi_tools import broadcast, mpi_avg, num_procs, proc_id, msg
+from .mpi_tools import broadcast, mpi_avg, num_procs, proc_id, msg, gather
 
 def setup_pytorch_for_mpi() -> None:
     """
@@ -64,3 +65,20 @@ def sync_params(module: nn.Module) -> None:
     for p in module.parameters():
         p_numpy = p.data.cpu().numpy()
         broadcast(p_numpy)
+
+def gather_loss_id(idx,loss):
+    if num_procs() == 1:
+        return idx,loss
+    buffer = np.array([idx,loss],dtype=np.float)
+    data = gather(buffer)
+    if data == None:
+        return None
+    else:
+        idxs = torch.tensor([int(data[i][0]) for i in range(num_procs())])
+        losses = torch.tensor([float(data[i][1]) for i in range(num_procs())])
+        return idxs, losses
+
+def sync_loss(loss_table):
+    if num_procs() == 1:
+        return
+    broadcast(loss_table)

plb/neurals/pcdataloader.py
@@ -14,7 +14,8 @@ class PointCloudDataset(Dataset):
         self.state_C = pointclouds['before_C']
         self.state_p = pointclouds['before_p']
         self.target_x = pointclouds['after_x']
-        self.loss = np.zeros(len(self.state_x),dtype=np.float)
+        self.loss_table = np.zeros(len(self.state_x),dtype=np.float)
+        np.random.seed(10)
         
     def __len__(self):
         return len(self.state_x)
@@ -34,13 +35,13 @@ class PointCloudDataset(Dataset):
 
     # All methods regardless of dataset or subdataset requires to set loss to here
     def recordLoss(self,idxs,losses):
-        self.loss[idxs] = losses
+        self.loss_table[idxs] = losses
 
     # Weighted Sampling from subset, no allow repeat?
     # Subset doesn't have weight since there is no need for more than one subsampling
     def getSubset(self,size,replace=False):
-        self.loss += 1e-5
-        subset_idx = np.random.choice(len(self.state_x),p=self.loss/self.loss.sum(),size=size,replace=replace)
+        self.loss_table += 1e-5
+        subset_idx = np.random.choice(len(self.state_x),p=self.loss_table/self.loss_table.sum(),size=size,replace=replace)
         sub_state_x = self.state_x[subset_idx]
         sub_state_v = self.state_v[subset_idx]
         sub_state_F = self.state_F[subset_idx]

plb/optimizer/focal_learn_latent.py
@@ -18,7 +18,7 @@ from ..engine.losses import state_loss, emd_loss, chamfer_loss, loss
 from ..engine.taichi_env import TaichiEnv
 from ..envs import make
 from ..neurals.autoencoder import PCNAutoEncoder
-from ..neurals.pcdataloader import ChopSticksDataset
+from ..neurals.pcdataloader import ChopSticksDataset,RopeDataset
 
 mpi.setup_pytorch_for_mpi()
 
@@ -108,6 +108,7 @@ class Solver:
         if not torch.isnan(x_hat_grad).any():
             return x_hat, x_hat_grad, loss_first, loss
         else:
+            mpi.msg("NAN Detected")
             return None, None, loss_first, loss
 
     # No grad version
@@ -169,7 +170,8 @@ def _loading_dataset()->DataLoader:
 
     :return: a dataloader of ChopSticksDataset
     """
-    dataset = ChopSticksDataset()
+    #dataset = ChopSticksDataset()
+    dataset = RopeDataset()
     dataloader = DataLoader(dataset,batch_size = mpi.num_procs())
     return dataloader, dataset
 
@@ -213,15 +215,20 @@ def _intialize_model(taichiEnv: TaichiEnv, device: torch.device)->PCNAutoEncoder
     :return: the intialized encoding model
     """
     model = PCNAutoEncoder(taichiEnv.n_particles, HIDDEN_LAYERS, LATENT_DIMS, FEAT_DMIS)
-    model.load_state_dict(torch.load("pretrain_model/lyh_model.pth")['net_state_dict'])
-    torch.save(model.encoder.state_dict(),'pretrain_model/emd_expert_encoder.pth')
+    model.load_state_dict(torch.load("pretrain_model/network_emd_finetune_rope.pth")['net_state_dict'])
+    #torch.save(model.encoder.state_dict(),'pretrain_model/emd_expert_encoder.pth')
     model = model.to(device)
     return model
 
 def _update_loss(index,loss,dataset):
-    dataset.recordLoss(index,loss)
+    data = mpi.gather_loss_id(index,loss)
+    if data != None:
+        idxs, losses = data
+        dataset.recordLoss(idxs,losses)
+
 
 def _update_focal_scheme(dataset,size=1000):
+    mpi.sync_loss(dataset.loss_table)
     subdataset = dataset.getSubset(size)
     dataloader = DataLoader(subdataset,batch_size=mpi.num_procs())
     return dataloader
@@ -319,6 +326,8 @@ def learn_latent_focal(
         procAvgLoss[i] /= efficientBatchCnt
         mpi.msg(f"Epoch:{i}, process-local average loss:{procAvgLoss[i]}")
         if i % 5==0:
+            
+
             dataloader = _update_focal_scheme(original_dataset,1000)
             use_grad[0] = True
         elif i%5 == 4:
@@ -331,5 +340,5 @@ def learn_latent_focal(
 
     if mpi.proc_id() == 0:
         # ONLY one proc can store the model
-        torch.save(model.state_dict(),"pretrain_model/focal_chopsticks.pth")
-        torch.save(model.encoder.state_dict(),"pretrain_model/focal_chopsticks_encoder.pth")
+        torch.save(model.state_dict(),"pretrain_model/focal_rope.pth")
+        torch.save(model.encoder.state_dict(),"pretrain_model/focal_rope_encoder.pth")